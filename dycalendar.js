89dede60de4b476dffe2699fa29061441af992b9
gasperpb <achillespb@hotmail.com>
2021-04-25 21:44:22 -0300

    :up:

--- dycalendar.js
+++ dycalendar.js
@@ -32,7 +32,7 @@
         //name of the months
         // caso queira colocar em português só mudar 
         monthName = {
-            full: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+            full: ['January', 'February', 'March', 'Abril', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
             mmm: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
         },
 
